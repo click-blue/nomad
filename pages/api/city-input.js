31582f382158f4cb8b438fefeec9aad02009526d
ChatBSV <ab@alanborger.com>
2023-09-25 22:34:33 +0200

    v2

--- pages/api/city-input.js
+++ pages/api/city-input.js
@@ -9,36 +9,37 @@ export default async function handler(req, res) {
     return res.status(405).end();
   }
 
-  const { country } = req.body;
+  const { city, country } = req.body;
   const headers = {
     'Authorization': `Bearer ${WEBFLOW_API_KEY}`,
     'Content-Type': 'application/json',
-    'Accept-Version': '1.0.0'
+    'Accept': 'application/json'
   };
 
   try {
-    console.log(`Attempting to create a new country: ${country}`);
+    const createCountryItem = async () => {
+      const collectionId = 'Your_Countries_Collection_ID_Here'; // Replace with your actual Collection ID for countries
+      const fieldData = { name: country };
 
-    const fieldData = { name: country };
-    const response = await fetch(`https://api.webflow.com/collections/6511b5541b122aea972eaf8f/items`, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify({
-        isArchived: false,
-        isDraft: false,
-        fieldData
-      })
-    });
+      const response = await fetch(`https://api.webflow.com/v2/collections/${collectionId}/items`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({
+          isArchived: false,
+          isDraft: false,
+          fieldData
+        })
+      });
 
-    const data = await response.json();
-    console.log(`Create country response:`, JSON.stringify(data, null, 2));
+      const data = await response.json();
+      if (response.ok) {
+        return res.status(200).json({ status: 'success', data });
+      } else {
+        return res.status(400).json({ status: 'failed', error: data });
+      }
+    };
 
-    if (data.id) {
-      return res.status(200).json({ status: 'success', id: data.id });
-    } else {
-      return res.status(400).json({ status: 'failed', error: data });
-    }
-
+    await createCountryItem();
   } catch (error) {
     console.log('Error:', JSON.stringify(error, null, 2));
     return res.status(500).json({ status: 'error', error: error.message });
