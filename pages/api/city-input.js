08a589dfaf745823e75b382b0219d5bb2e9fd0c0
ChatBSV <ab@alanborger.com>
2023-09-25 23:07:50 +0200

    0.1

--- pages/api/city-input.js
+++ pages/api/city-input.js
@@ -3,36 +3,70 @@
 import fetch from 'node-fetch';
 
 const WEBFLOW_API_KEY = process.env.WEBFLOW_API_KEY;
-const headers = {
-  'Authorization': `Bearer ${WEBFLOW_API_KEY}`,
-  'Content-Type': 'application/json',
-  'Accept': 'application/json'
-};
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
 
   const { city, country } = req.body;
-  if (!country) return res.status(400).json({ status: 'failed', error: 'Country name is required' });
+  const headers = {
+    'Authorization': `Bearer ${WEBFLOW_API_KEY}`,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+
+  const getOrCreateItem = async (collectionId, itemName, relatedCountryId = null) => {
+    let response = await fetch(`https://api.webflow.com/v2/collections/${collectionId}/items?limit=1&name=${itemName}`, {
+      headers: headers
+    });
+    let data = await response.json();
+
+    let itemId;
+    if (!data.items || data.items.length === 0) {
+      const fieldData = { name: itemName };
+      if (relatedCountryId) {
+        fieldData.country = relatedCountryId;
+      }
+
+      response = await fetch(`https://api.webflow.com/v2/collections/${collectionId}/items`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({
+          isArchived: false,
+          isDraft: false,
+          fieldData
+        })
+      });
+      data = await response.json();
+      itemId = data.id;
+
+      // Publish the item
+      response = await fetch(`https://api.webflow.com/v2/collections/${collectionId}/items/publish`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({
+          publishedItemIds: [itemId]
+        })
+      });
+      await response.json();
+    } else {
+      itemId = data.items[0].id;
+    }
+
+    return itemId;
+  };
 
   try {
-    const collectionId = '6511b5541b122aea972eaf8f';
-    const response = await fetch(`https://api.webflow.com/v2/collections/${collectionId}/items`, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify({
-        isArchived: false,
-        isDraft: false,
-        fieldData: { name: country }
-      })
-    });
+    // Step 1: Check and publish country first, and get its ID
+    const countryId = await getOrCreateItem('6511b5541b122aea972eaf8f', country);
 
-    const data = await response.json();
-    return response.ok 
-      ? res.status(200).json({ status: 'success', data }) 
-      : res.status(400).json({ status: 'failed', error: data });
+    // Step 2: Then check and publish city, linking it to the country
+    await getOrCreateItem('6511b5388842397b68f73aad', city, countryId);
+
+    return res.status(200).json({ status: 'success' });
   } catch (error) {
-    console.error('Error:', JSON.stringify(error, null, 2));
+    console.log('Error:', JSON.stringify(error, null, 2));
     return res.status(500).json({ status: 'error', error: error.message });
   }
 }
