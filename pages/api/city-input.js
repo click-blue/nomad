58c2b88bb9dad7fb8db9ef5ab4f6aed34488ee07
ChatBSV <ab@alanborger.com>
2023-09-25 20:04:31 +0200

    create cross referenced item and publish

--- pages/api/city-input.js
+++ pages/api/city-input.js
@@ -1,6 +1,5 @@
 // pages/api/city-input.js
 
-// Load environment variables
 const WEBFLOW_API_KEY = process.env.WEBFLOW_API_KEY;
 
 export default async function handler(req, res) {
@@ -9,7 +8,6 @@ export default async function handler(req, res) {
   }
 
   const { city, country } = req.body;
-
   const headers = {
     'Authorization': `Bearer ${WEBFLOW_API_KEY}`,
     'Content-Type': 'application/json',
@@ -17,40 +15,50 @@ export default async function handler(req, res) {
   };
 
   try {
-    // Search for country in the countries collection
-    let response = await fetch(`https://api.webflow.com/collections/6511b5541b122aea972eaf8f/items?limit=1&name=${country}`, {
-      headers: headers
-    });
-    let data = await response.json();
-
-    // If country doesn't exist, create it
-    if (!data.items || data.items.length === 0) {
-      await fetch(`https://api.webflow.com/collections/6511b5541b122aea972eaf8f/items`, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify({ name: country })
+    const getOrCreateItem = async (collectionId, itemName, relatedCountryId = null) => {
+      let response = await fetch(`https://api.webflow.com/collections/${collectionId}/items?limit=1&name=${itemName}`, {
+        headers: headers
       });
-    }
+      let data = await response.json();
 
-    // Search for city in the cities collection
-    response = await fetch(`https://api.webflow.com/collections/6511b5388842397b68f73aad/items?limit=1&name=${city}`, {
-      headers: headers
-    });
-    data = await response.json();
+      let itemId;
+      if (!data.items || data.items.length === 0) {
+        const fieldData = { name: itemName };
+        if (relatedCountryId) {
+          fieldData.country = relatedCountryId;
+        }
 
-    // If city doesn't exist, create it
-    if (!data.items || data.items.length === 0) {
-      await fetch(`https://api.webflow.com/collections/6511b5388842397b68f73aad/items`, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify({ name: city })
-      });
-    }
+        response = await fetch(`https://api.webflow.com/collections/${collectionId}/items`, {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({ fieldData })
+        });
+        data = await response.json();
+        itemId = data._id;
 
-    // If everything is successful, return a success status
+        // Publish the item
+        await fetch(`https://api.webflow.com/collections/${collectionId}/items/publish`, {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({
+            itemIds: [itemId]
+          })
+        });
+      } else {
+        itemId = data.items[0]._id;
+      }
+
+      return itemId;
+    };
+
+    // Check and publish country first, and get its ID
+    const countryId = await getOrCreateItem('6511b5541b122aea972eaf8f', country);
+
+    // Then check and publish city, linking it to the country
+    await getOrCreateItem('6511b5388842397b68f73aad', city, countryId);
+
     return res.status(200).json({ status: 'success' });
   } catch (error) {
-    // If there's an error, return an error status
     return res.status(500).json({ status: 'error', error: error.message });
   }
 }
