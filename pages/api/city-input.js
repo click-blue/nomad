3390c97bc18addf4d28900bd9322f972746354fb
ChatBSV <ab@alanborger.com>
2023-09-25 19:49:11 +0200

    country first

--- pages/api/city-input.js
+++ pages/api/city-input.js
@@ -17,32 +17,33 @@ export default async function handler(req, res) {
   };
 
   try {
-    // Search for city in the cities collection
-    let response = await fetch(`https://api.webflow.com/collections/6511b5388842397b68f73aad/items?limit=1&name=${city}`, {
+    // Search for country in the countries collection
+    let response = await fetch(`https://api.webflow.com/collections/6511b5541b122aea972eaf8f/items?limit=1&name=${country}`, {
       headers: headers
     });
     let data = await response.json();
-    
-    // If city doesn't exist, create it
-    if (data.items.length === 0) {
-      await fetch(`https://api.webflow.com/collections/6511b5388842397b68f73aad/items`, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify({ name: city })
-      });
-    }
-    
-    // Repeat the same logic for the countries collection
-    response = await fetch(`https://api.webflow.com/collections/6511b5541b122aea972eaf8f/items?limit=1&name=${country}`, {
-      headers: headers
-    });
-    data = await response.json();
-    
-    if (data.items.length === 0) {
+
+    // If country doesn't exist, create it
+    if (!data.items || data.items.length === 0) {
       await fetch(`https://api.webflow.com/collections/6511b5541b122aea972eaf8f/items`, {
         method: 'POST',
         headers: headers,
         body: JSON.stringify({ name: country })
+      });
+    }
+
+    // Search for city in the cities collection
+    response = await fetch(`https://api.webflow.com/collections/6511b5388842397b68f73aad/items?limit=1&name=${city}`, {
+      headers: headers
+    });
+    data = await response.json();
+
+    // If city doesn't exist, create it
+    if (!data.items || data.items.length === 0) {
+      await fetch(`https://api.webflow.com/collections/6511b5388842397b68f73aad/items`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({ name: city })
       });
     }
 
